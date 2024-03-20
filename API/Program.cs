586f2a0944ff9b47ea98745ed1d185a2462f0e3e
Prashansa Sinha <61187353+prashansa358@users.noreply.github.com>
2024-03-20 19:43:50 +0530

    End of setting up angular cli

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DataContext>(opt=>{
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -24,6 +25,7 @@ var app = builder.Build();
 //     app.UseSwaggerUI();
 // }
 
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
