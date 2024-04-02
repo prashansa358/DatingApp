47b09e9aed5e81ac5b4187de303e49931fa35a89
Prashansa Sinha <61187353+prashansa358@users.noreply.github.com>
2024-04-02 17:11:36 +0530

    End of section for adding routing to navbar

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
 model:any ={};
 // currentUser$: Observable<User |null> = of(null);
 // loggedIn = false;
-constructor(public accountService:AccountService){}
+constructor(public accountService:AccountService,private router:Router, private toastr:ToastrService){}
 ngOnInit(): void {
   // this.currentUser$ = this.accountService.currentUser$;
 //  this.getCurrentUser();
@@ -28,14 +30,16 @@ ngOnInit(): void {
 login(){
   this.accountService.login(this.model).subscribe({
     next: () =>{
+      this.router.navigateByUrl('/members');
       // console.log(response);
       // this.loggedIn = true;
     },
-    error: error => console.log(error)
+    error: error => this.toastr.error(error.error)
   })
 }
 logout(){
   this.accountService.logout();
+  this.router.navigateByUrl('/')
   // this.loggedIn = false;
 }
 }
